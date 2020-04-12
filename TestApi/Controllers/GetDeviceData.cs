cde5b61032d9d7a493fc3d4c59d338969077c043
Lukkan <patrik@pilonlogic.se>
2020-04-12 14:28:00 +0200

    Attribute routing added

---
+++ TestApi/Controllers/GetDeviceData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("api/{apiVersion}/device/{deviceId}/data/{date}/{sensorType?}")]
+    public class GetDeviceData : ControllerBase
+    {
+        private readonly ILogger<GetDeviceData> _logger;
+
+        public GetDeviceData(ILogger<GetDeviceData> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DummyClass> Get(string apiVersion, string deviceId, string date, string? sensorType)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new DummyClass
+            {
+                id = index,
+                api = apiVersion
+            })
+            .ToArray();
+        }
+        public class DummyClass
+        {
+            public int id { get; set; }
+            public string api { get; set; }
+
+        }
+    }
+}

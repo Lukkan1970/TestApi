c2b37adf6aec7136f6b1bd7d40397914eeecbc85
Lukkan <patrik@pilonlogic.se>
2020-04-13 11:25:19 +0200

    Minor changes to AzureBlobRetriever adn model Devices.cs added

--- TestApi/AzureBlobFunctions/AzureBlobRetriever.cs
+++ TestApi/AzureBlobFunctions/AzureBlobRetriever.cs
@@ -4,6 +4,7 @@ using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 
 namespace TestApi.AzureBlobFunctions
@@ -12,20 +13,18 @@ namespace TestApi.AzureBlobFunctions
     public class AzureBlobRetriever
     {
         private static CloudBlobClient _Client = null;
+        private static string accountName = "sigmaiotexercisetest";
+        private static string SAS = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rl&se=2028-09-27T16:27:24Z&st=2018-09-27T08:27:24Z&spr=https&sig=eYVbQneRuiGn103jUuZvNa6RleEeoCFx1IftVin6wuA%3D";
+
+
 
         public static void Class_Init()
         {
-
             //var saName = _Context.Properties["AzureStorageAccountName"] as string;
             //var saKey = _Context.Properties["AzureStorageAccountAccessKey1"] as string;
-
-
-            var storageCredentials = new StorageCredentials("?sv=2017-11-09&ss=bfqt&srt=sco&sp=rl&se=2028-09-27T16:27:24Z&st=2018-09-27T08:27:24Z&spr=https&sig=eYVbQneRuiGn103jUuZvNa6RleEeoCFx1IftVin6wuA%3D");
-            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, "sigmaiotexercisetest", endpointSuffix: null, useHttps: true);
+            var storageCredentials = new StorageCredentials(SAS);
+            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, accountName, endpointSuffix: null, useHttps: true);
             _Client = cloudStorageAccount.CreateCloudBlobClient();
-
-
-
         }
 
 
--- TestApi/Models/Devices.cs
+++ TestApi/Models/Devices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,7 @@ namespace TestApi.Models
 {
     public class Devices
     {
+        public string DeviceId { get; set; }
+        public IEnumerable<string> SensorTypes { get; set; }
     }
 }
